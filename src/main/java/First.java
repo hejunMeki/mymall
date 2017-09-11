39b69177108ec62276476825d46896732361d643
hejun <hejunMeki@users.noreply.github.com>
2017-09-11 10:32:19 +0800

    first commit

---
+++ src/main/java/First.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/9/11 0011.
+ * Description:...
+ */
+public class First {
+    public static void main(String args[])
+    {
+        System.out.println("first class");
+    }
+}
