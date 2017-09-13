7d21a1d8ccf0b69ae94f03bd713aff278eb66780
hejun <hejunMeki@users.noreply.github.com>
2017-09-13 22:17:20 +0800

    all config commit

---
+++ src/main/java/com/mymall/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.mymall.dao;
+
+import com.mymall.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
